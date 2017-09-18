6569285f339131cd6b098dded5cc9acb13b58673
Amit Jena <amitjena.viz@gmail.com>
2017-09-18 06:43:52 +0530

    Railway Connectivity
    Shows the presence of at least one train between the state capitals. This information is shown by a line connecting them.

---
+++ data/stops.js
@@ -0,0 +1,35 @@
+var stops = {
+    "type": "FeatureCollection",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+
+    "features": [
+    { "type": "Feature", "properties": { "name": "Hyderabad", "lat": 17.39998313, "lng": 78.47995357 }, "geometry": { "type": "Point", "coordinates": [ 78.47995357, 17.39998313 ] } },
+    { "type": "Feature", "properties": { "name": "Dispur", "lat": 26.14402305, "lng": 91.76663611 }, "geometry": { "type": "Point", "coordinates": [ 91.76663611, 26.14402305 ] } },
+    { "type": "Feature", "properties": { "name": "Patna", "lat": 25.62495913, "lng": 85.13003861 }, "geometry": { "type": "Point", "coordinates": [ 85.13003861, 25.62495913 ] } },
+    { "type": "Feature", "properties": { "name": "Raipur", "lat": 21.23499453, "lng": 81.63500647 }, "geometry": { "type": "Point", "coordinates": [ 81.63500647, 21.23499453 ] } },
+    { "type": "Feature", "properties": { "name": "Panaji", "lat": 15.491997, "lng": 73.81800065 }, "geometry": { "type": "Point", "coordinates": [ 73.81800065, 15.491997 ] } },
+    { "type": "Feature", "properties": { "name": "Gandhinagar", "lat": 23.233086, "lng": 72.651634 }, "geometry": { "type": "Point", "coordinates": [ 72.651634, 23.233086 ] } },
+    { "type": "Feature", "properties": { "name": "Chandigarh", "lat": 30.71999697, "lng": 76.78000565 }, "geometry": { "type": "Point", "coordinates": [ 76.78000565, 30.71999697 ] } },
+    { "type": "Feature", "properties": { "name": "Ranchi", "lat": 23.37000633, "lng": 85.33002641 }, "geometry": { "type": "Point", "coordinates": [ 85.33002641, 23.37000633 ] } },
+    { "type": "Feature", "properties": { "name": "Bangalore", "lat": 12.96999514, "lng": 77.56000972 }, "geometry": { "type": "Point", "coordinates": [ 77.56000972, 12.96999514 ] } },
+    { "type": "Feature", "properties": { "name": "Thiruvananthapuram", "lat": 8.499983743, "lng": 76.95002112 }, "geometry": { "type": "Point", "coordinates": [ 76.95002112, 8.499983743 ] } },
+    { "type": "Feature", "properties": { "name": "Bhopal", "lat": 23.24998781, "lng": 77.40999304 }, "geometry": { "type": "Point", "coordinates": [ 77.40999304, 23.24998781 ] } },
+    { "type": "Feature", "properties": { "name": "Mumbai", "lat": 19.01699038, "lng": 72.8569893 }, "geometry": { "type": "Point", "coordinates": [ 72.8569893, 19.01699038 ] } },
+    { "type": "Feature", "properties": { "name": "Bhubaneshwar", "lat": 20.27042808, "lng": 85.82736039 }, "geometry": { "type": "Point", "coordinates": [ 85.82736039, 20.27042808 ] } },
+    { "type": "Feature", "properties": { "name": "Jaipur", "lat": 26.92113324, "lng": 75.80998734 }, "geometry": { "type": "Point", "coordinates": [ 75.80998734, 26.92113324 ] } },
+    { "type": "Feature", "properties": { "name": "Chennai", "lat": 13.08998781, "lng": 80.27999874 }, "geometry": { "type": "Point", "coordinates": [ 80.27999874, 13.08998781 ] } },
+    { "type": "Feature", "properties": { "name": "Agartala", "lat": 23.83540428, "lng": 91.27999914 }, "geometry": { "type": "Point", "coordinates": [ 91.27999914, 23.83540428 ] } },
+    { "type": "Feature", "properties": { "name": "Dehra Dun", "lat": 30.32040895, "lng": 78.05000565 }, "geometry": { "type": "Point", "coordinates": [ 78.05000565, 30.32040895 ] } },
+    { "type": "Feature", "properties": { "name": "Lucknow", "lat": 26.85503908, "lng": 80.91499874 }, "geometry": { "type": "Point", "coordinates": [ 80.91499874, 26.85503908 ] } },
+    { "type": "Feature", "properties": { "name": "Kolkata", "lat": 22.572645, "lng": 88.363892 }, "geometry": { "type": "Point", "coordinates": [ 88.363892, 22.572645 ] } },
+    { "type": "Feature", "properties": { "name": "New Delhi", "lat": 28.60002301, "lng": 77.19998002 }, "geometry": { "type": "Point", "coordinates": [ 77.19998002, 28.60002301 ] } },
+    { "type": "Feature", "properties": { "name": "Itanagar", "lat": 27.10039878, "lng": 93.61660071 }, "geometry": { "type": "Point", "coordinates": [ 93.61660071, 27.10039878 ] } },
+    { "type": "Feature", "properties": { "name": "Shimla", "lat": 31.10002545, "lng": 77.16659704 }, "geometry": { "type": "Point", "coordinates": [ 77.16659704, 31.10002545 ] } },
+    { "type": "Feature", "properties": { "name": "Srinagar", "lat": 34.09997154, "lng": 74.81500932 }, "geometry": { "type": "Point", "coordinates": [ 74.81500932, 34.09997154 ] } },
+    { "type": "Feature", "properties": { "name": "Imphal", "lat": 24.79997072, "lng": 93.95001705 }, "geometry": { "type": "Point", "coordinates": [ 93.95001705, 24.79997072 ] } },
+    { "type": "Feature", "properties": { "name": "Shillong", "lat": 25.57049217, "lng": 91.8800142 }, "geometry": { "type": "Point", "coordinates": [ 91.8800142, 25.57049217 ] } },
+    { "type": "Feature", "properties": { "name": "Aizawl", "lat": 23.71039899, "lng": 92.72001461 }, "geometry": { "type": "Point", "coordinates": [ 92.72001461, 23.71039899 ] } },
+    { "type": "Feature", "properties": { "name": "Kohima", "lat": 25.6669979, "lng": 94.11657019 }, "geometry": { "type": "Point", "coordinates": [ 94.11657019, 25.6669979 ] } },
+    { "type": "Feature", "properties": { "name": "Gangtok", "lat": 27.3333303, "lng": 88.6166475 }, "geometry": { "type": "Point", "coordinates": [ 88.6166475, 27.3333303 ] } }
+]
+};
